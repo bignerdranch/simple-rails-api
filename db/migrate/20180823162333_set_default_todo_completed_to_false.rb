05ddccba12161240a2333c78dc9c4e124e92410f
Josh Justice <me@codingitwrong.com>
2018-08-23 12:23:51 -0400

    Default completion flag to false
    rails generate migration set_default_todo_completed_to_false

---
+++ db/migrate/20180823162333_set_default_todo_completed_to_false.rb
@@ -0,0 +1,5 @@
+class SetDefaultTodoCompletedToFalse < ActiveRecord::Migration[5.2]
+  def change
+    change_column_default :todos, :complete, from: nil, to: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_161901) do
+ActiveRecord::Schema.define(version: 2018_08_23_162333) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
-    t.boolean "complete"
+    t.boolean "complete", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
