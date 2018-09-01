aba9be6643a6efe94ba54a0391cfcb33528e0729
Josh Justice <me@codingitwrong.com>
2018-09-01 19:15:17 -0400

    Generate note
    rails generate model note body:text todo:references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_163723) do
+ActiveRecord::Schema.define(version: 2018_09_01_231415) do
+
+  create_table "notes", force: :cascade do |t|
+    t.text "body"
+    t.integer "todo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_id"], name: "index_notes_on_todo_id"
+  end
 
   create_table "oauth_access_grants", force: :cascade do |t|
     t.integer "resource_owner_id", null: false
