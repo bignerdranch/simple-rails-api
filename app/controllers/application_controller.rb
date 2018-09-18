0abe125f12ab90d4871ae21e83ca128f2fbcce39
Josh Justice <me@codingitwrong.com>
2018-09-18 06:56:48 -0400

    Provide way to retrieve current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+
+  private
+
+  def current_user
+    @current_user ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
 end
